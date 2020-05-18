084fc79dda57142b4cc8264b3c3ca2e8cc54d5a9
Alexey Naumov <alexsnaumov@gmail.com>
2020-05-18 14:36:28 +0300

    Init commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+
+
+    // Accordion in FAQ
+    $(function() {
+        var Accordion = function(el, multiple) {
+            this.el = el || {};
+            this.multiple = multiple || false;
+
+            var links = this.el.find('.info__item-toggle');
+            links.on('click', {
+                el: this.el,
+                multiple: this.multiple
+            }, this.dropdown)
+        };
+
+        Accordion.prototype.dropdown = function(e) {
+            var $el = e.data.el;
+            $this = $(this),
+                $next = $this.next();
+
+            $next.slideToggle();
+            $this.parent().toggleClass('open');
+
+            if (!e.data.multiple) {
+                $el.find('.info__item-inner').not($next).slideUp().parent().removeClass('open');
+            };
+        };
+        var accordion = new Accordion($('.info__list'), false);
+    });
+
+});
